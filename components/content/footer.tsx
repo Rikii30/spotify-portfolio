44cd893edd48af9073b3852f1161462bb7a8cfa2
Rikii30 <112274714+Rikii30@users.noreply.github.com>
2024-03-03 19:09:05 -0600

    Update footer.tsx

--- components/content/footer.tsx
+++ components/content/footer.tsx
@@ -1,6 +1,5 @@
 import { memo } from 'react';
-import { PlayCircle, SkipBack, SkipForward, Shuffle, Volume2 } from 'react-feather'; // Example SVG icons, you can replace them with suitable icons/components
-
+import { PlayCircle, SkipBack, SkipForward, Shuffle, Volume2 } from 'react-feather';
 import socials from '../../data/socials';
 
 const Footer: React.FC = memo(() => {
@@ -9,38 +8,23 @@ const Footer: React.FC = memo(() => {
       <div className="hidden md:block flex-1"></div>
       <div className="flex md:justify-center flex-1">
         <div className="flex items-center">
-          <div
-            className="transform hover:scale-105 hover:text-green-spotify transition duration-200 mx-2 md:mx-3"
-          >
+          <div className="transform hover:scale-105 hover:text-green-spotify transition duration-200 mx-2 md:mx-3">
             <Shuffle strokeWidth={1.5} size={18} />
           </div>
-          <div
-            className="transform hover:scale-105 hover:text-green-spotify transition duration-200 mx-2 md:mx-3"
-          >
-        
+          <div className="transform hover:scale-105 hover:text-green-spotify transition duration-200 mx-2 md:mx-3">
             <SkipBack strokeWidth={1.5} size={18} />
           </div>
-          <div
-            className="transform hover:scale-105 hover:text-green-spotify transition duration-200 mx-2 md:mx-3"
-          >
-          
+          <div className="transform hover:scale-105 hover:text-green-spotify transition duration-200 mx-2 md:mx-3">
             <PlayCircle strokeWidth={1.5} size={18} />
           </div>
-          <div
-            className="transform hover:scale-105 hover:text-green-spotify transition duration-200 mx-2 md:mx-3"
-          >
+          <div className="transform hover:scale-105 hover:text-green-spotify transition duration-200 mx-2 md:mx-3">
             <SkipForward strokeWidth={1.5} size={18} />
           </div>
-          <div
-            className="transform hover:scale-105 hover:text-green-spotify transition duration-200 mx-2 md:mx-3"
-          >
+          <div className="transform hover:scale-105 hover:text-green-spotify transition duration-200 mx-2 md:mx-3">
             <Volume2 strokeWidth={1.5} size={18} />
           </div>
           {socials.map(({ id, icon: Icon, url }) => (
-            <div
-              key={id}
-              className="transform hover:scale-105 hover:text-green-spotify transition duration-200 mx-2 md:mx-3"
-            >
+            <div key={id} className="transform hover:scale-105 hover:text-green-spotify transition duration-200 mx-2 md:mx-3">
               <a href={url} target="_blank" rel="noreferrer">
                 <Icon strokeWidth={1.5} size={18} />
               </a>
@@ -49,11 +33,13 @@ const Footer: React.FC = memo(() => {
         </div>
       </div>
       <div className="text-xs text-right flex-1">
-        <div>Rikshitha's Portfolio</div>
-        
+        <div>Rikshithas Portfolio</div>
       </div>
     </footer>
   );
 });
+
+// Add display name to component
+Footer.displayName = 'Footer';
 
 export default Footer;
