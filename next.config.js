d3f543fcabb7d6df8f82632c1c2925619656e5cc
Rikii30 <112274714+Rikii30@users.noreply.github.com>
2024-03-03 18:45:50 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 const nextConfig = {
-  output: 'export',
+  output: 'standalone', // Change output to 'standalone' as per the allowed values
   distDir: 'dist',
-  images: {
-    unoptimized: true,
-  },
+  // Remove the experimental 'images' option if not needed
 };
 
 module.exports = nextConfig;
