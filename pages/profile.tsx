08da4b761d1680d80fc3e97292143d812f780d93
Rikii30 <112274714+Rikii30@users.noreply.github.com>
2024-03-03 19:08:38 -0600

    Update profile.tsx

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -18,13 +18,12 @@ const Profile: NextPage = () => {
         <Section>
           <SectionTitle title="About me" />
           <p className="text-base">
-          A creative coder with a deep passion for both art and technology. With a background in painting and a love for crafting webpages, I thrive at the intersection of creativity and technical prowess. My curiosity extends beyond just coding – I'm also intrigued by the insights data analysis can unveil. During my university years, I combined these interests to create Symphony Spot, a web application where users can discover new music, curated playlists, and even purchase vinyl records. Music has always been a significant part of my life; from learning Indian classical Carnatic music to curating playlists on Spotify, it's a constant source of inspiration. My dream is to curate a vast vinyl collection, showcasing the favorite artists of both myself and my friends. Beyond my technical skills, I'm known for being friendly and outgoing, valuing collaboration and strong relationships within a team. I'm eager to bring my unique blend of creativity, technical expertise, and love for music to the Spotify team, and I'm excited to gain more experience and knowledge.
+          A creative coder with a deep passion for both art and technology. With a background in painting and a love for crafting webpages, I thrive at the intersection of creativity and technical prowess. My curiosity extends beyond just coding I &apos;m also intrigued by the insights data analysis can unveil. During my university years, I combined these interests to create Symphony Spot, a web application where users can discover new music, curated playlists, and even purchase vinyl records. Music has always been a significant part of my life; from learning Indian classical Carnatic music to curating playlists on Spotify, it&apos;s a constant source of inspiration. My dream is to curate a vast vinyl collection, showcasing the favorite artists of both myself and my friends. Beyond my technical skills, I&apos;m known for being friendly and outgoing, valuing collaboration and strong relationships within a team. I&apos;m eager to bring my unique blend of creativity, technical expertise, and love for music to the Spotify team, and I&apos;m excited to gain more experience and knowledge.
           <br />
           <br />
           <hr />
 
             <div className="mt-4">
-            <a href="https://rikii30.github.io/project-1/home.html" style={{ color: 'green' }} target="_blank">Click here to view my project inspired by Spotify</a>
 
             </div>
             <div className="mt-4">Certifications:</div>
