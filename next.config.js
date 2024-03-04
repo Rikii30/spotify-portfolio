8ca04b6f617605a5aa9faafc36486757b0f2a91c
Rikii30 <112274714+Rikii30@users.noreply.github.com>
2024-03-03 19:20:15 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 const nextConfig = {
   output: 'standalone', // Change output to 'standalone' as per the allowed values
   distDir: 'dist',
-  images:{
-    unoptimized: true,
+  images: {
+    domains: ['example.com'],
+    deviceSizes: [320, 420, 768, 1024, 1200], // Optional: Specify device sizes for image optimization
+    imageSizes: [16, 32, 48, 64, 96], // Optional: Specify image sizes for image optimization
+    loader: 'default', // Optional: Specify custom image loader (e.g., 'imgix', 'cloudinary', 'akamai')
   },
+  
   // Remove the experimental 'images' option if not needed
 };
 
